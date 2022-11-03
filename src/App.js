7d589f36013539fe769929187b370fb0b6909813
Dan4422 <sdlee095@gmail.com>
2022-11-02 23:21:05 -0400

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [color, setColor] = useState('black')
+
+  const incrementCount = () => {
+    setCount(count+1)
+  }
+
+  useEffect(() => {
+    const changeColorOnClick = () => {
+      if (color === 'black') {
+        setColor('red')
+      } else {
+        setColor('black')
+      }
+    }
+
+    document.addEventListener('click', changeColorOnClick)
+
+    return () => {
+      document.removeEventListener('click', changeColorOnClick)
+    }
+  }, [color])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <div>{count}</div>
+      <button onClick={incrementCount}>Increment</button> */}
+      <div
+        id='myDiv'
+        style={{
+          color:'white',
+          width: '100px',
+          height: '100px',
+          position: 'absolute',
+          left: '50%',
+          right: '50%',
+          backgroundColor: color,
+        }}
+      >
+        This div can change color. Click on me!
+      </div>
     </div>
   );
 }
